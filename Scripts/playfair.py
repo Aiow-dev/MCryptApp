1b9dadc611650da1c2151f955c0720e85622b9bf
Ilya Tsybulenok <75560146+Aiow-dev@users.noreply.github.com>
2022-10-09 17:36:33 +0300

    fix-dec-ps

--- Scripts/playfair.py
+++ Scripts/playfair.py
@@ -1,3 +1,4 @@
+import itertools
 from Assets import constants
 
 from Scripts import base_scripts
@@ -12,12 +13,9 @@ def enc_playfair(msg, key_word, size_a=4, size_b=8):
     enc_table = base_scripts.sum_unique(key_word, enc_table)
     enc_table = base_scripts.sum_unique(table_alphabet, enc_table)
     table = [[0] * size_b for _ in range(size_a)]
-    counter = 0
     enc_msg = ''
-    for i in range(size_a):
-        for j in range(size_b):
-            table[i][j] = enc_table[counter % 32]
-            counter += 1
+    for counter, (i, j) in enumerate(itertools.product(range(size_a), range(size_b))):
+        table[i][j] = enc_table[counter % 32]
     for i in range(0, len(msg), 2):
         first = base_scripts.find(table, msg[i], size_a, size_b)
         second = base_scripts.find(table, msg[i + 1], size_a, size_b)
@@ -36,17 +34,16 @@ def enc_playfair(msg, key_word, size_a=4, size_b=8):
 
 def dec_playfair(enc_msg, key_word, size_a=4, size_b=8):
     enc_msg = enc_msg.replace(' ', '').lower()
+    if len(enc_msg) % 2 == 1:
+        enc_msg += "ъ"
     table_alphabet = constants.CUT_RU_ALPHABET
     enc_table = ''
     enc_table = base_scripts.sum_unique(key_word, enc_table)
     enc_table = base_scripts.sum_unique(table_alphabet, enc_table)
     table = [[0] * size_b for _ in range(size_a)]
-    counter = 0
     msg = ''
-    for i in range(size_a):
-        for j in range(size_b):
-            table[i][j] = enc_table[counter % 32]
-            counter += 1
+    for counter, (i, j) in enumerate(itertools.product(range(size_a), range(size_b))):
+        table[i][j] = enc_table[counter % 32]
     for i in range(0, len(enc_msg), 2):
         first = base_scripts.find(table, enc_msg[i], size_a, size_b)
         second = base_scripts.find(table, enc_msg[i + 1], size_a, size_b)
